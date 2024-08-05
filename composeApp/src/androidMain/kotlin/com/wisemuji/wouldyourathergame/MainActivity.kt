16d17f4ebea8d21f2dfdf89aa85267e203daaea5
wisemuji <wisemuji@gmail.com>
2024-08-05 17:46:40 +0900

    feat: apply edge to edge screen

--- composeApp/src/androidMain/kotlin/com/wisemuji/wouldyourathergame/MainActivity.kt
+++ composeApp/src/androidMain/kotlin/com/wisemuji/wouldyourathergame/MainActivity.kt
@@ -4,11 +4,13 @@ import ui.App
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.tooling.preview.Preview
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+        enableEdgeToEdge()
         super.onCreate(savedInstanceState)
 
         setContent {
@@ -17,8 +19,3 @@ class MainActivity : ComponentActivity() {
     }
 }
 
-@Preview
-@Composable
-fun AppAndroidPreview() {
-    App()
-}
