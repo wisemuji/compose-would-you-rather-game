2674a5903950bd72a1d2f5ba18263e1f683cd4a5
wisemuji <wisemuji@gmail.com>
2024-08-11 15:58:12 +0900

    style: remove unnecessary comments

--- composeApp/src/commonMain/kotlin/model/Option.kt
+++ composeApp/src/commonMain/kotlin/model/Option.kt
@@ -3,10 +3,3 @@ package model
 enum class Option {
     A, B
 }
-
-//sealed interface Option {
-//    val text: String
-//
-//    data class A(override val text: String) : Option
-//    data class B(override val text: String) : Option
-//}
